0f5ab9ce368b384b7aee09753962f77112853c6a
jamesonBradfield <jamesonbradfield@gmail.com>
2025-06-08 18:25:17 -0600

    fixed cannon rotation

--- scenes/cannon.gd
+++ scenes/cannon.gd
@@ -5,6 +5,7 @@ extends Node2D
 
 var level
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	level = get_node("/root/level")
@@ -12,16 +13,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var mouse_position = get_viewport().get_global_mouse_position()
-	look_at(mouse_position)
-	
+	rotate(get_angle_to(get_global_mouse_position()))
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("launch_kitten"):
 		launch_kitten()
 
+
 func launch_kitten():
 	var kitten = kittenScene.instantiate()
 	kitten.position = spawn_position.global_position()
-	kitten.velocity = Vector2(100,100) # update values to depend on mouse cursor position
+	kitten.velocity = Vector2(100, 100)  # update values to depend on mouse cursor position
 	level.add_child(kitten)
